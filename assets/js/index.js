3617dea0c4cb7174cec6fea927edba0a3bbd1b76
MNlegion <rkreuser30@gmail.com>
2023-09-01 23:12:26 -0400

    inital search functionality setup

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,15 @@
 // logic for pokemon api calls
+
+var getPokemon = function(pokemon) {
+    // format the pokeapi url
+    var apiUrl = "https://pokeapi.co/api/v2/pokemon/" + pokemon;
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+};
+
+getPokemon("mew");
