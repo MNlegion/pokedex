214346d0cdcf4fd5ff0e5bfb30053e5911089124
MNlegion <rkreuser30@gmail.com>
2023-09-02 20:17:54 -0400

    continued logic for displaying user selected pokemon

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,22 @@
+var pokemonFormEl = document.querySelector("#pokemon-form");
+var pokemonInputEl = document.querySelector("#pokemon");
+var pokedexContainerEl = document.querySelector("#pokedex-container");
+var pokemonSearchTerm = document.querySelector("#pokemon-search-term");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+
+    var pokemonName = pokemonInputEl.value.trim();
+
+    if (pokemonName) {
+        getPokemon(pokemonName);
+        pokemonInputEl.value = "";
+    } else {
+        alert("Please enter a Pokemon Name");
+    }
+    console.log(event);
+};
+
 // logic for pokemon api calls
 
 var getPokemon = function(pokemon) {
@@ -7,9 +26,18 @@ var getPokemon = function(pokemon) {
     // make a request to the url
     fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
-            console.log(data);
+            displayPokemon(data, pokemon)
         });
     });
 };
 
-getPokemon("mew");
+var displayPokemon = function(pokedexEntry, searchTerm) {
+    if (pokedexEntry.length === 0) {
+        pokedexContainerEl.textContent = "No Pokemon Found.";
+        return;
+    }
+    
+    pokemonSearchTerm.textContent = searchTerm;
+};
+
+pokemonFormEl.addEventListener("submit", formSubmitHandler);
