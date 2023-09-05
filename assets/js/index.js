6c63d45627be762827cf18eeb5cf299d25011067
MNlegion <rkreuser30@gmail.com>
2023-09-04 20:52:13 -0400

    continued logic for user input

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,58 +1,77 @@
-var pokemonFormEl = document.querySelector("#pokemon-form");
-var pokemonInputEl = document.querySelector("#pokemon");
-var pokedexContainerEl = document.querySelector("#pokedex-container");
-var pokemonSearchTerm = document.querySelector("#pokemon-search-term");
+// var pokemonFormEl = document.querySelector("#pokemon-form");
+// var pokemonInputEl = document.querySelector("#pokemon");
+// var pokedexContainerEl = document.querySelector("#pokedex-container");
+// var pokemonSearchTerm = document.querySelector("#pokemon-search-term");
 
-var formSubmitHandler = function(event) {
-    event.preventDefault();
+// var formSubmitHandler = function (event) {
+//   event.preventDefault();
 
-    var pokemonName = pokemonInputEl.value.trim();
+//   var pokemonName = pokemonInputEl.value.trim();
 
-    if (pokemonName) {
-        getPokemon(pokemonName);
-        pokemonInputEl.value = "";
-    } else {
-        alert("Please enter a Pokemon Name");
-    }
-    console.log(event);
-};
+//   if (pokemonName) {
+//     getPokemon(pokemonName);
+//     pokemonInputEl.value = "";
+//   } else {
+//     alert("Please enter a Pokemon Name");
+//   }
+// };
 
-// logic for pokemon api calls
+// // logic for pokemon api calls
+
+// var getPokemon = function (pokemon) {
+//   // format the pokeapi url
+//   var apiUrl = "https://pokeapi.co/api/v2/pokemon/" + pokemon;
+
+//   // make a request to the url
+//   fetch(apiUrl).then(function (response) {
+//     response.json().then(function (data) {
+//     //   displayPokemon(data, pokemon);
+//     });
+//   });
+// };
+
+// var displayPokemon = function (pokedexEntry, searchTerm) {
+//   if (pokedexEntry.length === 0) {
+//     pokedexContainerEl.textContent = "No Pokemon Found.";
+//     return;
+//   }
+
+//   // console.log("Height: " + pokedexEntry.height + " meters");
+//   pokemonSearchTerm.textContent = searchTerm;
+
+//   for (var i = 0; i < pokedexEntry.length; i++) {
+
+//     var statsName = pokedexEntry[i];
+
+//     // container to hold pokemon data
+//     var containerEl = document.createElement("div");
+//     containerEl.classList =
+//       "list-item flex-row justify space-beteen align-center";
+
+//     // span element to hold pokemon name
+//     var titleEl = document.createElement("span");
+//     titleEl.textContent = statsName[i];
+
+//     // append to container
+//     containerEl.appendChild(titleEl);
+
+//     // append container to dom
+//     pokedexContainerEl.appendChild(containerEl);
+//   }
+// };
+
+// pokemonFormEl.addEventListener("submit", formSubmitHandler);
+
 
 var getPokemon = function(pokemon) {
-    // format the pokeapi url
+
     var apiUrl = "https://pokeapi.co/api/v2/pokemon/" + pokemon;
 
-    // make a request to the url
     fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
-            displayPokemon(data, pokemon)
+            console.log(data.abilities[2].ability);
         });
     });
 };
 
-var displayPokemon = function(pokedexEntry, searchTerm) {
-    if (pokedexEntry.length === 0) {
-        pokedexContainerEl.textContent = "No Pokemon Found.";
-        return;
-    }
-    
-    console.log(pokedexEntry.sprites.other.dream_world.front_default);
-    pokemonSearchTerm.textContent = searchTerm;
-
-    // container to hold pokemon data
-    var containerEl = document.createElement("div");
-    containerEl.classList = "list-item flex-row justify space-beteen align-center";
-
-    // span element to hold pokemon name
-    var titleEl = document.createElement("span");
-    titleEl.textContent = searchTerm;
-
-    // append to container
-    containerEl.appendChild(titleEl);
-
-    // append container to dom
-    pokedexContainerEl.appendChild(containerEl);
-};
-
-pokemonFormEl.addEventListener("submit", formSubmitHandler);
+getPokemon("chansey");
